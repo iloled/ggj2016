826dadfb2a1a359262741b2df8014d7e171cfb12
dany <dany.siriphol@gmail.com>
2016-01-30 10:41:08 +0100

    Added actions

---
+++ Assets/script_dany/Actions/MoveAction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+public class MoveAction : Action
+{
+
+	private int position;
+	private NPC npc;
+
+
+	public MoveAction (Player p, NPC npc,  int position)
+	{
+		base.p = p;
+		this.position = position;
+		this.npc = npc;
+	}
+
+	protected override void behaviour()
+	{
+		npc.position = position;
+	}
+}
+
+
---
+++ Assets/script_dany/Party.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Party {
+	public List<NPC> members = new List<NPC>();
+}
