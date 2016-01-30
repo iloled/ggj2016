fa595c3300c223c62fc0d91c15ab969f5e2f7f66
yann <yannpassy1@gmail.com>
2016-01-31 00:23:52 +0100

    action convertion

--- Assets/script_dany/Player.cs
+++ Assets/script_dany/Player.cs
@@ -21,6 +21,16 @@ public class Player {
 		Debug.Log (this.name + "Moving");
 		actionNumber--;
 	}
+
+	// convertion de NPC
+	public void convertWarrior(NPC npc){
+		if (ressource.gold >= 2 && ressource.blood >= 1) {
+			ressource.gold -= 2;
+			ressource.blood--;
+
+
+		}
+	}
 		
 
 }
