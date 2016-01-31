802f979f448698ed4787e717481c6e15576b0982
yann <yannpassy1@gmail.com>
2016-01-31 01:03:20 +0100

    Player.cs ajout des fonction convertion

--- Assets/script_dany/Player.cs
+++ Assets/script_dany/Player.cs
@@ -22,15 +22,57 @@ public class Player {
 		actionNumber--;
 	}
 
-	// convertion de NPC
+	 
+	// =============   Convertion des NPC  ============== //
+
 	public void convertWarrior(NPC npc){
 		if (ressource.gold >= 2 && ressource.blood >= 1) {
 			ressource.gold -= 2;
 			ressource.blood--;
 
-
+			int position = npc.position;
+			npc = new Warrior ();
+			npc.setPosition (position);
+			this.party.addNPC (npc);
 		}
 	}
-		
+
+
+	public void convertArcher(NPC npc){
+		if ( ressource.blood >= 1 && ressource.holyWater>=2) {
+			ressource.blood --;
+			ressource.holyWater -=2;
+
+			int position = npc.position;
+			npc = new Archer ();
+			npc.setPosition (position);
+			this.party.addNPC (npc);
+		}
+	}	
+
+
+	public void convertMage(NPC npc){
+		if (ressource.holyWater >= 1 && ressource.blood >= 2) {
+			ressource.blood -= 2;
+			ressource.holyWater--;
+
+			int position = npc.position;
+			npc = new Mage ();
+			npc.setPosition (position);
+			this.party.addNPC (npc);
+		}
+	}
+
+
+	public void converttThief(NPC npc){
+		if (ressource.gold >= 3) {
+			ressource.gold -= 3;
+
+			int position = npc.position;
+			npc = new Thief ();
+			npc.setPosition (position);
+			this.party.addNPC (npc);
+		}
+	}
 
 }
