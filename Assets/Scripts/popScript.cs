26df53136927bc59b21a95b9d010c0e09b0635f1
iannzor <fitamant.iann@gmail.com>
2016-01-31 13:52:21 +0100

    ajout

---
+++ Assets/Scripts/popScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class popScript : MonoBehaviour {
+
+	[SerializeField]
+	Transform[] prefabPop;
+
+
+	public void popFire(int TileID)
+	{
+		var x = TileID%32;
+		var y = TileID/32;
+		
+		Instantiate (prefabPop [0], new Vector3 (1 + x, 1 + y, 0.0f), Quaternion.identity);
+	}
+
+	public void popConvert(int TileID)
+	{
+		var x = TileID%32;
+		var y = TileID/32;
+		
+		Instantiate (prefabPop [1], new Vector3 (1 + x, 1 + y, 0.0f), Quaternion.identity);
+	}
+
+	public void popSlash(int TileID)
+	{
+		var x = TileID%32;
+		var y = TileID/32;
+		
+		Instantiate (prefabPop [2], new Vector3 (1 + x, 1 + y, 0.0f), Quaternion.identity);
+	}
+
+	/*void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Z))
+		{
+			popFire(33);
+		}
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			popConvert(33);
+		}
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			popSlash(33);
+		}
+	}*/
+
+
+
+
+
+}
